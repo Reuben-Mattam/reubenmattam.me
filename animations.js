a89a98a1c5bb5db0622d7cd1e3cf187d9b0cdb6c
Reuben <reubenrjmattam@gmail.com>
2025-07-12 22:51:37 +1000

    test1 of new portfolio

--- animations.js
+++ animations.js
@@ -1,3 +1,9 @@
+  window.addEventListener('scroll', () => {
+    if (window.scrollX !== 0) {
+      window.scrollTo(0, window.scrollY);
+    }
+  });
+
 function updateScrollbarWidth() {
     const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
     document.documentElement.style.setProperty('--scrollbar-width', `${scrollbarWidth}px`);
@@ -7,58 +13,13 @@ updateScrollbarWidth();
 
 window.addEventListener('resize', updateScrollbarWidth);
 
-/* Logo Change Animation !! */
-document.addEventListener('DOMContentLoaded', function () {
-    let navText = document.querySelector('.nav-text');
-    
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                navText.style.opacity = 0;
-                setTimeout(() => {
-                    navText.textContent = 'Reuben Mattam';
-                    navText.style.opacity = 1;
-                }, 500);
-            } else {
-                navText.style.opacity = 0;
-                setTimeout(() => {
-                    navText.textContent = 'https://reubenmattam.me';
-                    navText.style.opacity = 1;
-                }, 500);
-            }
-        });
-    }, {
-        threshold: 0.1
+/* nav bar animation */
+const menu = document.getElementById("navbar");
+
+Array.from(document.getElementsByClassName("nav-item"))
+    .forEach((item, index) => {
+        item.onmouseover = () => {
+            menu.dataset.activeIndex = index;
+        }
     });
-
-    document.querySelectorAll('.not-start').forEach(section => {
-        observer.observe(section);
-    });
-});
-/* Logo Change Animation !! */
-
-/* Hambuger (yum)*/
-function toggleMenu() {
-    const navRight = document.querySelector('.nav-right');
-    navRight.classList.toggle('active');
-}
-
-function toggleMenu() {
-    const navRight = document.querySelector('.nav-right');
-    navRight.classList.toggle('active');
-}
-/* Hambuger (yum)*/
-
-/* Resuize nav */
-function updateNavText() {
-    const navText = document.querySelector('.nav-text');
-    if (window.innerWidth < 800) {
-        navText.textContent = 'Reuben Mattam';
-    } else {
-        navText.textContent = 'https://reubenmattam.me';
-    }
-}
-
-window.addEventListener('load', updateNavText);
-window.addEventListener('resize', updateNavText);
-/* Resuize nav */
+/* nav bar animation*/

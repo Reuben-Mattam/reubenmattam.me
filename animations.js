ca162ea5d0db927cf7d41058bc67dc1f6e593bc4
Reuben <reubenrjmattam@gmail.com>
2025-07-15 15:27:28 +1000

    updated portfolio part 2

--- animations.js
+++ animations.js
@@ -13,13 +13,62 @@ updateScrollbarWidth();
 
 window.addEventListener('resize', updateScrollbarWidth);
 
-/* nav bar animation */
-const menu = document.getElementById("navbar");
+/* home page animation via intersection observer api */
 
-Array.from(document.getElementsByClassName("nav-item"))
-    .forEach((item, index) => {
-        item.onmouseover = () => {
-            menu.dataset.activeIndex = index;
-        }
+/* home page animation */
+
+/* Logo Change Animation !! */
+document.addEventListener('DOMContentLoaded', function () {
+    let navText = document.querySelector('.nav-text');
+    
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                navText.style.opacity = 0;
+                setTimeout(() => {
+                    navText.textContent = 'Reuben Mattam';
+                    navText.style.opacity = 1;
+                }, 500);
+            } else {
+                navText.style.opacity = 0;
+                setTimeout(() => {
+                    navText.textContent = 'https://reubenmattam.me';
+                    navText.style.opacity = 1;
+                }, 500);
+            }
+        });
+    }, {
+        threshold: 0.1
     });
-/* nav bar animation*/
+
+    document.querySelectorAll('.not-start').forEach(section => {
+        observer.observe(section);
+    });
+});
+/* Logo Change Animation !! */
+
+/* Hambuger (yum)*/
+function toggleMenu() {
+    const navRight = document.querySelector('.nav-right');
+    navRight.classList.toggle('active');
+}
+
+function toggleMenu() {
+    const navRight = document.querySelector('.nav-right');
+    navRight.classList.toggle('active');
+}
+/* Hambuger (yum)*/
+
+/* Resuize nav */
+function updateNavText() {
+    const navText = document.querySelector('.nav-text');
+    if (window.innerWidth < 800) {
+        navText.textContent = 'Reuben Mattam';
+    } else {
+        navText.textContent = 'https://reubenmattam.me';
+    }
+}
+
+window.addEventListener('load', updateNavText);
+window.addEventListener('resize', updateNavText);
+/* Resuize nav */
